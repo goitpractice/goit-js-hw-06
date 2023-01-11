dbd071dda6d643f30d9b5b6e267cb8733a035085
unknown <Taras>
2023-01-11 12:26:07 +0100

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
+/*
+Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку
+на button.change-color і виводить значення кольору в span.color.
+*/
+
+const changeColorButtonEl = document.querySelector("button");
+const currentColorEl = document.querySelector(".color");
+
+changeColorButtonEl.addEventListener("click", () => {
+  const color = getRandomHexColor();
+
+  currentColorEl.textContent = color;
+  document.body.setAttribute("style", `background-color: ${color}`);
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
